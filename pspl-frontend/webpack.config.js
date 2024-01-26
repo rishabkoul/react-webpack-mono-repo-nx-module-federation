b65775a96a7ada0e0a00f26b9535191b96dc65fb
rishab_koul <t-rishab.koul@paytmmoney.com>
2024-01-26 16:51:19 +0530

    webpack may need in future

--- pspl-frontend/webpack.config.js
+++ pspl-frontend/webpack.config.js
@@ -13,4 +13,16 @@ module.exports = composePlugins(
   withNx(),
   withReact(),
   withModuleFederation(webPackconfig)
+  // (config) => {
+  //   // Update the webpack config as needed here.
+  //   // e.g. `config.plugins.push(new MyPlugin())`
+
+  //   // This will properly handle imported svg's
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack', 'url-loader'],
+  //   });
+
+  //   return config;
+  // }
 );
